a8e0623305d92e62dfc775dedfe6c37bd7a2e88c
Tung Bui <tungbq.info@gmail.com>
2021-08-13 22:38:42 +0700

    Render markdown and sanitize html

--- models/article.js
+++ models/article.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 const marked = require('marked')
 const slugify = require('slugify')
 
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+
 const articleSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -22,12 +26,21 @@ const articleSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true
   }
 })
 
 articleSchema.pre('validate', function (next) {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true })
+  }
+
+  if (this.markdown) {
+    // convert markdown to html, and purify html
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
   }
 
   next()
